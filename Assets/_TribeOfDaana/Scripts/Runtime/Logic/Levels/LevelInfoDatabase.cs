7ab44b65b346f46165e58ae35cb8896d2a1c2d0d
Haiito92 <hannaantoinebusiness@gmail.com>
2025-09-24 15:41:40 +0200

    [GP] LevelInfoData base

---
+++ Assets/_TribeOfDaana/Scripts/Runtime/Logic/Levels/LevelInfoDatabase.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace _TribeOfDaana.Scripts.Runtime.Logic.Levels
+{
+    [CreateAssetMenu(fileName = "FILENAME", menuName = "MENUNAME", order = 0)]
+    public class LevelInfoDatabase : ScriptableObject
+    {
+        
+    }
+}
